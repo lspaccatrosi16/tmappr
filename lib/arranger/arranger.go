627a3f82c3908b86985780b836eecd2e1fc8bf00
Luca Spaccatrosi <l.spaccatrosi@outlook.com>
2024-03-28 13:48:42 +0000

    line weighting

--- lib/arranger/arranger.go
+++ lib/arranger/arranger.go
@@ -10,26 +10,86 @@ import (
 type genLine struct {
 	Line     *types.Line
 	Segments []*types.CompoundSegment
+	WSegs    []weightSeg
+}
+
+type weightSeg struct {
+	Start   int
+	End     int
+	Segment *types.CompoundSegment
 }
 
 func (g *genLine) String() string {
 	segText := []string{}
-	for _, s := range g.Segments {
-		segText = append(segText, s.LineSegment.String())
+	if len(g.WSegs) > 0 {
+		for _, s := range g.WSegs {
+			segText = append(segText, s.Segment.LineSegment.String()+fmt.Sprintf(" %d:%d", s.Start, s.End))
+		}
+	} else {
+		for _, s := range g.Segments {
+			segText = append(segText, s.LineSegment.String())
+		}
 	}
 	return fmt.Sprintf("%s: %s", g.Line.Code, strings.Join(segText, ", "))
 }
 
 func Arrange(config *types.AppConfig, data *types.AppData) {
 	idivlines := regenLines(data)
+	calculateWeights(idivlines)
 
 	for _, line := range idivlines {
 		fmt.Println(line.String())
 	}
 }
 
-func regenLines(data *types.AppData) []genLine {
-	lines := []genLine{}
+func calculateWeights(lines []*genLine) {
+	for _, line := range lines {
+		wsegs := []weightSeg{}
+		for i := 0; i < len(line.Segments); i++ {
+			seg := line.Segments[i]
+			wseg := weightSeg{
+				Segment: seg,
+			}
+			if i > 0 {
+				var weight int
+				weighta := seg.Gradient.NumberCw(line.Segments[i-1].Gradient)
+				weightb := seg.Gradient.NumberAcw(line.Segments[i-1].Gradient)
+				if weighta < weightb {
+					weight = weighta
+				} else {
+					weight = weightb
+				}
+
+				if weight > 4 {
+					weight = 4 - weight
+				}
+				wseg.Start = weight
+			}
+
+			if i < len(line.Segments)-1 {
+				var weight int
+				weighta := seg.Gradient.NumberCw(line.Segments[i+1].Gradient)
+				weightb := seg.Gradient.NumberAcw(line.Segments[i+1].Gradient)
+				if weighta < weightb {
+					weight = weighta
+				} else {
+					weight = weightb
+				}
+
+				if weight > 4 {
+					weight = 4 - weight
+				}
+				wseg.End = weight
+			}
+
+			wsegs = append(wsegs, wseg)
+		}
+		line.WSegs = wsegs
+	}
+}
+
+func regenLines(data *types.AppData) []*genLine {
+	lines := []*genLine{}
 
 	for _, line := range data.Lines {
 		gl := genLine{
@@ -66,7 +126,7 @@ func regenLines(data *types.AppData) []genLine {
 				}
 			}
 		}
-		lines = append(lines, gl)
+		lines = append(lines, &gl)
 	}
 
 	return lines
